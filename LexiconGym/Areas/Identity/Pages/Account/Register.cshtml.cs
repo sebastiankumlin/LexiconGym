59b721f3fc8859f21cc4f25cc4e8988def3a7f42
DZM <d.bjorlingh@gmail.com>
2019-06-13 16:15:27 +0200

    Register to role Member default

--- LexiconGym/Areas/Identity/Pages/Account/Register.cshtml.cs
+++ LexiconGym/Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -20,17 +20,20 @@ namespace LexiconGym.Areas.Identity.Pages.Account
         private readonly UserManager<ApplicationUser> _userManager;
         private readonly ILogger<RegisterModel> _logger;
         private readonly IEmailSender _emailSender;
+        private readonly RoleManager<IdentityRole> roleManager;
 
         public RegisterModel(
             UserManager<ApplicationUser> userManager,
             SignInManager<ApplicationUser> signInManager,
             ILogger<RegisterModel> logger,
-            IEmailSender emailSender)
+            IEmailSender emailSender
+           )
         {
             _userManager = userManager;
             _signInManager = signInManager;
             _logger = logger;
             _emailSender = emailSender;
+            this.roleManager = roleManager;
         }
 
         [BindProperty]
@@ -75,8 +78,9 @@ namespace LexiconGym.Areas.Identity.Pages.Account
             {
                 //Spara Namnet till AppliciationUser 4/4
                 var user = new ApplicationUser { UserName = Input.Email, Email = Input.Email, Name = Input.Name };
-                var result = await _userManager.CreateAsync(user, Input.Password);
-                if (result.Succeeded)
+                var resultAddUser = await _userManager.CreateAsync(user, Input.Password);
+                var resultAddRole = await _userManager.AddToRoleAsync(user, "Member");
+                if (resultAddUser.Succeeded && resultAddRole.Succeeded)
                 {
                     _logger.LogInformation("User created a new account with password.");
 
@@ -93,7 +97,7 @@ namespace LexiconGym.Areas.Identity.Pages.Account
                     await _signInManager.SignInAsync(user, isPersistent: false);
                     return LocalRedirect(returnUrl);
                 }
-                foreach (var error in result.Errors)
+                foreach (var error in resultAddUser.Errors)
                 {
                     ModelState.AddModelError(string.Empty, error.Description);
                 }
